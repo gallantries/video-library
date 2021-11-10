bf52f5f2899778aa633844c5e4a17aeefa4e23e8
Helena Rasche <hxr@hx42.org>
2021-11-10 12:09:55 +0100

    fix

--- docs/bin/fetch-gtn-api.sh
+++ docs/bin/fetch-gtn-api.sh
@@ -1,5 +1,5 @@
 for topic in $(curl https://training.galaxyproject.org/training-material/api/topics.json | jq .[].name -r); do
 	curl https://training.galaxyproject.org/training-material/api/topics/$topic.json | \
-		jq '.materials[] | ["/topics/" + .topic_name + "/tutorials/" + .tutorial_name, .title] | @tsv' -r | \
+		jq '.materials[] | ["/topics/" + .topic_name + "/tutorials/" + .tutorial_name, "\"" + .title + "\""] | @tsv' -r | \
 		sed 's/\t/: /g' >> out.yaml
 done;
