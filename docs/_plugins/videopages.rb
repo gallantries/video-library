21820a9c54a952329a1bf4ed2f5af76aed01fdcc
Saskia Hiltemann <zazkia@gmail.com>
2021-11-24 11:11:08 +0100

    add session pages

--- docs/_plugins/videopages.rb
+++ docs/_plugins/videopages.rb
@@ -29,6 +29,35 @@ module Jekyll
           site.pages << page2
         end
       end
+
+      if site.layouts.key? 'session'
+        dir = 'sessions'
+
+        site.data['sessions'].each_key do |session|
+          # Using PageWithoutAFile instead of a custom class which reads files
+          # from disk each time, saves some time, but it is unclear how much
+          # due to how the previous was accounted. But assuming 0.040s per page * 193 should be about 8 seconds.
+          page2 = PageWithoutAFile.new(site, "", File.join(dir, session), "index.html")
+          page2.content = nil
+          id2 = session.split('/')[0,2].join('/')
+
+          if site.data['gtn'].has_key?(id2)
+            name = site.data['gtn'].fetch(id2)
+          else
+            name = session
+          end if
+
+          # Their tutorials
+          page2.data['session'] = session
+          page2.data['sessionname'] = name
+          page2.data['title'] = "GTN Video: #{name}"
+          page2.data['layout'] = "session"
+
+          site.pages << page2
+        end
+      end
+
+
     end
   end
 end
