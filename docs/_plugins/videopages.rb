d09609bb8fe9ac0f7d677c66901521ecda6a73e4
Saskia Hiltemann <shiltemann@users.noreply.github.com>
2021-11-24 11:40:08 +0100

    Update docs/_plugins/videopages.rb
    Co-authored-by: Helena <helena.rasche@gmail.com>

--- docs/_plugins/videopages.rb
+++ docs/_plugins/videopages.rb
@@ -34,9 +34,6 @@ module Jekyll
         dir = 'sessions'
 
         site.data['sessions'].each_key do |session|
-          # Using PageWithoutAFile instead of a custom class which reads files
-          # from disk each time, saves some time, but it is unclear how much
-          # due to how the previous was accounted. But assuming 0.040s per page * 193 should be about 8 seconds.
           page2 = PageWithoutAFile.new(site, "", File.join(dir, session), "index.html")
           page2.content = nil
           id2 = session.split('/')[0,2].join('/')
