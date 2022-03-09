274881d02ff5b62e41eff310a89725b22f0cc57d
Helena Rasche <hxr@hx42.org>
2022-03-09 14:08:55 +0100

    ok, done templating

--- slack-utils/markdown2mrkdwn.py
+++ slack-utils/markdown2mrkdwn.py
@@ -4,13 +4,6 @@ import marko
 import marko.ast_renderer
 
 
-with open(sys.argv[1], 'r') as handle:
-    markdown = marko.Markdown(renderer=marko.ast_renderer.ASTRenderer)
-    doc = markdown.convert(handle.read())
-
-blocks = []
-blocks_obj = {'blocks': blocks}
-
 def render_paragraph(children):
     text = ""
     for kid in children:
@@ -26,118 +19,134 @@ def render_paragraph(children):
             raise Exception(f"Unhandled: {kid}")
     return text
 
-for kid in doc['children']:
-    if kid['element'] == 'heading':
-        blocks.append({
-            'type': 'header',
-            'text': {
-                'type': 'plain_text',
-                'text': kid['children'][0]['children'],
-            }
-        })
-    elif kid['element'] == 'blank_line':
-        continue
-    elif kid['element'] == 'paragraph':
-        blocks.append({
-            'type': 'section',
-            'text': {
-                'type': 'mrkdwn',
-                'text': render_paragraph(kid['children']),
-            }
-        })
-    elif kid['element'] == 'quote':
-        blocks.append({
-            'type': 'section',
-            'text': {
-                'type': 'mrkdwn',
-                'text': '> ' + render_paragraph(kid['children'][0]['children']),
-            }
-        })
-    elif kid['element'] == 'list':
-        text = ""
-        for idx, list_element in enumerate(kid['children']):
-            list_item = list_element['children']
-            text += '• ' if kid['ordered'] is False else f"{idx}. "
-            text += render_paragraph(list_item[0]['children']) + '\n'
 
-        blocks.append({
-            'type': 'section',
-            'text': {
-                'type': 'mrkdwn',
-                'text': text,
-            }
-        })
-    elif kid['element'] == 'thematic_break':
-        blocks.append({
-            'type': 'divider',
-        })
-    elif kid['element'] == 'html_block':
-        text = kid['children'].strip()
-        if text == '<SERVERS>':
+def convert_text(text):
+    markdown = marko.Markdown(renderer=marko.ast_renderer.ASTRenderer)
+    doc = markdown.convert(text)
+    return convert_markodoc(doc)
+
+
+def convert_markodoc(doc):
+    blocks = []
+    blocks_obj = {'blocks': blocks}
+
+    for kid in doc['children']:
+        if kid['element'] == 'heading':
             blocks.append({
-                "type": "actions",
-                "elements": [
-                    {
-                        "type": "button",
-                        "text": {
-                            "type": "plain_text",
-                            "text": "UseGalaxy.eu :earth_africa:",
-                        },
-                        "url": "https://usegalaxy.eu/"
-                    },
-                    {
-                        "type": "button",
-                        "text": {
-                            "type": "plain_text",
-                            "text": "UseGalaxy.org :earth_americas:",
-                        },
-                        "url": "https://usegalaxy.org/"
-                    },
-                    {
-                        "type": "button",
-                        "text": {
-                            "type": "plain_text",
-                            "text": "UseGalaxy.org.au :earth_asia:",
-                        },
-                        "url": "https://usegalaxy.org.au/"
-                    },
-                ]
+                'type': 'header',
+                'text': {
+                    'type': 'plain_text',
+                    'text': kid['children'][0]['children'],
+                }
             })
-        elif text == '<TIAAS>':
+        elif kid['element'] == 'blank_line':
+            continue
+        elif kid['element'] == 'paragraph':
             blocks.append({
-                "type": "actions",
-                "elements": [
-                    {
-                        "type": "button",
-                        "text": {
-                            "type": "plain_text",
-                            "text": "UseGalaxy.eu :earth_africa:",
-                        },
-                        "url": "https://usegalaxy.eu/join-training/gtn-tapas"
-                    },
-                    {
-                        "type": "button",
-                        "text": {
-                            "type": "plain_text",
-                            "text": "UseGalaxy.org :earth_americas:",
-                        },
-                        "url": "https://usegalaxy.org/join-training/gtn-tapas"
-                    },
-                    {
-                        "type": "button",
-                        "text": {
-                            "type": "plain_text",
-                            "text": "UseGalaxy.org.AU :earth_asia:",
-                        },
-                        "url": "https://usegalaxy.org.au/join-training/gtn-tapas"
-                    },
-                ]
+                'type': 'section',
+                'text': {
+                    'type': 'mrkdwn',
+                    'text': render_paragraph(kid['children']),
+                }
             })
+        elif kid['element'] == 'quote':
+            blocks.append({
+                'type': 'section',
+                'text': {
+                    'type': 'mrkdwn',
+                    'text': '> ' + render_paragraph(kid['children'][0]['children']),
+                }
+            })
+        elif kid['element'] == 'list':
+            text = ""
+            for idx, list_element in enumerate(kid['children']):
+                list_item = list_element['children']
+                text += '• ' if kid['ordered'] is False else f"{idx}. "
+                text += render_paragraph(list_item[0]['children']) + '\n'
+
+            blocks.append({
+                'type': 'section',
+                'text': {
+                    'type': 'mrkdwn',
+                    'text': text,
+                }
+            })
+        elif kid['element'] == 'thematic_break':
+            blocks.append({
+                'type': 'divider',
+            })
+        elif kid['element'] == 'html_block':
+            text = kid['children'].strip()
+            if text == '<SERVERS>':
+                blocks.append({
+                    "type": "actions",
+                    "elements": [
+                        {
+                            "type": "button",
+                            "text": {
+                                "type": "plain_text",
+                                "text": "UseGalaxy.eu :earth_africa:",
+                            },
+                            "url": "https://usegalaxy.eu/"
+                        },
+                        {
+                            "type": "button",
+                            "text": {
+                                "type": "plain_text",
+                                "text": "UseGalaxy.org :earth_americas:",
+                            },
+                            "url": "https://usegalaxy.org/"
+                        },
+                        {
+                            "type": "button",
+                            "text": {
+                                "type": "plain_text",
+                                "text": "UseGalaxy.org.au :earth_asia:",
+                            },
+                            "url": "https://usegalaxy.org.au/"
+                        },
+                    ]
+                })
+            elif text == '<TIAAS>':
+                blocks.append({
+                    "type": "actions",
+                    "elements": [
+                        {
+                            "type": "button",
+                            "text": {
+                                "type": "plain_text",
+                                "text": "UseGalaxy.eu :earth_africa:",
+                            },
+                            "url": "https://usegalaxy.eu/join-training/gtn-tapas"
+                        },
+                        {
+                            "type": "button",
+                            "text": {
+                                "type": "plain_text",
+                                "text": "UseGalaxy.org :earth_americas:",
+                            },
+                            "url": "https://usegalaxy.org/join-training/gtn-tapas"
+                        },
+                        {
+                            "type": "button",
+                            "text": {
+                                "type": "plain_text",
+                                "text": "UseGalaxy.org.AU :earth_asia:",
+                            },
+                            "url": "https://usegalaxy.org.au/join-training/gtn-tapas"
+                        },
+                    ]
+                })
+            else:
+                raise Exception(f"Cannot handle {kid['children']}")
+
+
         else:
-            raise Exception(f"Cannot handle {kid['children']}")
+            raise Exception(f"Cannot handle {kid}")
+
+    return json.dumps(blocks_obj)
 
 
-    else:
-        raise Exception(f"Cannot handle {kid}")
-
-print(json.dumps(blocks_obj))
+if __name__ == '__main__':
+    with open(sys.argv[1], 'r') as handle:
+        print(convert_text(handle.read()))
