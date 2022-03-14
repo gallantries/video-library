923fab950d05d51b689be8edfd9fdab0a9eec1a3
Helena Rasche <hxr@hx42.org>
2022-03-14 11:24:33 +0100

    tracking

--- slack-utils/list-users.py
+++ slack-utils/list-users.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python
+from collections import Counter
 import argparse
 import json
-import os
-from slack_bolt import App
 import time
 import lib
 
 
-app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
 tz = {}
-for member in lib.list_users(app):
+emoji = Counter()
+for member in lib.list_users():
     t = member.get('tz_offset', 0) / 3600
     if t not in tz:
         tz[t] = 0
     tz[t] += 1
 
+    if 'profile' in member:
+        if 'status_emoji' in member['profile']:
+            emoji[member['profile']['status_emoji']] += 1
+
 __import__('pprint').pprint(tz)
+
+__import__('pprint').pprint(emoji)
