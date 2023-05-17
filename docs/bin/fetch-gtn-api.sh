f2003de89d7ad42182d98658dadd0684f2b005b5
Helena Rasche <hexylena@galaxians.org>
2023-05-17 12:46:14 +0200

    add supported servers to UI

--- docs/bin/fetch-gtn-api.sh
+++ docs/bin/fetch-gtn-api.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 out=$(mktemp -d)
 for topic in $(curl https://training.galaxyproject.org/training-material/api/topics.json | jq .[].name -r); do
-	curl https://training.galaxyproject.org/training-material/api/topics/$topic.json > "${out}/gtn_${topic}.json";
+	curl https://training.galaxyproject.org/training-material/api/topics/$topic.json | jq -S > "${out}/gtn_${topic}.json";
 done;
 
 python3 bin/objectives.py "${out}"/gtn*.json
