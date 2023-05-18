17d6e991c361232d4fe9ca62b385955fe23c5dc6
hexylena <hexylena@users.noreply.github.com>
2023-05-18 08:59:03 +0000

    deploy: 6bed2ec04353a0987c4473a203eaf289efac04d3

--- bin/fetch-gtn-api.sh
+++ bin/fetch-gtn-api.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 out=$(mktemp -d)
+cli=""
 for topic in $(curl https://training.galaxyproject.org/training-material/api/topics.json | jq .[].name -r); do
-	curl https://training.galaxyproject.org/training-material/api/topics/$topic.json > "${out}/gtn_${topic}.json";
+	cli="$cli https://training.galaxyproject.org/training-material/api/topics/$topic.json -o ${out}/gtn_${topic}.json"
 done;
+curl --parallel $cli
 
 python3 bin/objectives.py "${out}"/gtn*.json
--- bin/objectives.py
+++ bin/objectives.py
@@ -5,6 +5,7 @@ import json
 objectives = {}
 studyload = {}
 gtn = {}
+supported = {}
 
 for fn in sorted(sys.argv[1:]):
     with open(fn, 'r') as handle:
@@ -15,18 +16,23 @@ for fn in sorted(sys.argv[1:]):
 
             gtn[k] = material['title']
             studyload[k] = material.get('time_estimation', None)
+            supported[k] = material.get('supported_servers', None)
 
             if material.get('slides', False):
                 objectives[k + '/slides'] = material.get('objectives', [])
 
             if material.get('hands_on', False):
                 objectives[k + '/tutorial'] = material.get('objectives', [])
+                supported[k + '/tutorial'] = material.get('supported_servers', None)
 
 with open('_data/gtn.json', 'w') as handle:
-    json.dump(gtn, handle, ensure_ascii=False)
+    json.dump(gtn, handle, ensure_ascii=False, sort_keys=True, indent=2)
 
 with open('_data/objectives.json', 'w') as handle:
-    json.dump(objectives, handle, ensure_ascii=False)
+    json.dump(objectives, handle, ensure_ascii=False, sort_keys=True, indent=2)
 
 with open('_data/studyload.json', 'w') as handle:
-    json.dump(studyload, handle, ensure_ascii=False)
+    json.dump(studyload, handle, ensure_ascii=False, sort_keys=True, indent=2)
+
+with open('_data/supported.json', 'w') as handle:
+    json.dump(supported, handle, ensure_ascii=False, sort_keys=True, indent=2)
